758d912b61dfb186fd2cbee2d276bde8a838f9a3
DivakarSai <divakarsainukala@gmail.com>
2024-04-02 19:44:48 +0530

    taking scrolling region from detection

--- detection/region.py
+++ detection/region.py
@@ -1,4 +1,6 @@
 # store the scrolling region coordinates in a dictionary
 
-x,y,w,h = (0, 0, 100, 100)
-scrolling_region = {'x': x, 'y': y, 'w': w, 'h': h}
+x,y,w,h = (300, 1450, 2550, 150)
+scrolling_region = (x, y, w, h)
+
+
