b6f03bbfbc6be0af08dbbdede4b2e368ee3ebc74
DivakarSai <divakarsainukala@gmail.com>
2024-04-09 17:24:33 +0530

    finish off

--- generateFrame.py
+++ generateFrame.py
@@ -2,15 +2,18 @@ import cv2
 
 
 def generate_image_with_bounding_box(frame, scrolling_region):
+    
+    # Create a new image path
+    image_path = "images/sample_image.jpg" 
     # Draw a bounding box on the frame
     x, y, w, h = scrolling_region
     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    # Save the frame as an image
-    image_path = "images/sample_image.jpg"  # Replace with the desired path to save the image
-    cv2.imwrite(image_path, frame)
-    
-    return image_path
+
+    try:
+        cv2.imwrite(image_path, frame)
+        return image_path
+    except Exception as e:
+        raise Exception(f"Error saving image: {e}")
 
 
 
